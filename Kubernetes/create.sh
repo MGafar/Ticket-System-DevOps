e3abbf0ef240b8550657960c6743b5db99cb67b2
Ubuntu <ubuntu@ip-172-31-47-214.eu-west-2.compute.internal>
2021-04-30 16:05:47 +0000

    Connected back-end pods to RDS instance

--- Kubernetes/create.sh
+++ Kubernetes/create.sh
@@ -1,9 +1,23 @@
-#!/bin/bash
-# Create kubernetes NetworkLoadBalancer, Ingress, SpringBoot pods + service, ReactJS pods + service 
+#!/bin/bash  
+
+cp ./back-end.yaml /tmp/back-end.yaml
+
+# Get user inputs for RDS   
+echo "Please enter the RDS URL in the for (endpoint:port) : "
+read rds_url
+sed -i -e "s/rdsurlplaceholder/$rds_url/g" /tmp/back-end.yaml
+
+echo "Please enter the RDS username : "
+read rds_username
+sed -i -e "s/rdsusernameplaceholder/$rds_username/g" /tmp/back-end.yaml
+
+echo "Please enter the RDS password : "
+read rds_password
+sed -i -e "s/rdspasswordplaceholder/$rds_password/g" /tmp/back-end.yaml
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.45.0/deploy/static/provider/aws/deploy.yaml
 
-kubectl apply -f ./back-end.yaml
+kubectl apply -f /tmp/back-end.yaml
 
 kubectl apply -f ./back-end-service.yaml
 
